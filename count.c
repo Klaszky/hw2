fefae197a1fd0ef5a4914ed8c1ce615703fd6f7a
Joseph <jfklaszky@gmail.com>
2016-10-12 19:04:56 -0400

    testing some stuff

--- count.c
+++ count.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char argv[])
 {
+	char *address = "0x7f1a9804ae19";
+	char *end;
+	long test;
+	test = strtol(address, &end, 10);
+	 
+	printf("%s\n", address);
+	printf("%l\n", test);
 	return 0;
 }
 
